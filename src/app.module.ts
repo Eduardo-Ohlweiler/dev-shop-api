a7b27039a686fccefd6ebec3672f238c2220ff09
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-06-17 20:25:19 -0300

    chore: Conexão com a base de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
 import modules from './modules';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const host     = config.get<string>("DB_HOST")          || 'localhost';
+        const port     = Number(config.get<string>("DB_PORT"))  || 5432;
+        const username = config.get<string>("DB_USER")          || 'postgres';
+        const password = config.get<string>("DB_PASSWORD");
+        const database = config.get<string>("DB_NAME")          || 'dev_shop';
+        const ambiente = config.get<string>("NODE_ENV");
+        return {
+          type: "postgres",
+          host,
+          port,
+          username,
+          password,
+          database,
+          autoLoadEntities: true,
+          synchronize: ambiente !== 'prod'
+        }
+      }
+    }),
     ...modules
   ],
 })
